802adeefedf85bdd32cd6d84e4a34af4fdb63683
JC Brand <jc@opkode.com>
2015-01-22 18:35:18 +0100

    The init method has been moved here from registry.js
    This init method calls the constructor for the pattern and then initializes the
    return instance.
    
    This is Mockup specific and therefore shouldn't be done in the Patternslib
    registry.

--- js/pattern.js
+++ js/pattern.js
@@ -4,8 +4,9 @@
 
 define([
   'jquery',
-  'pat-registry'
-], function($, Registry) {
+  'pat-registry',
+  "pat-logger"
+], function($, Registry, logger) {
   'use strict';
 
   // Base Pattern
@@ -13,14 +14,16 @@ define([
     this.$el = $el;
     this.options = $.extend(true, {}, this.defaults || {}, options || {});
     this.init();
-    this.trigger('init');
+    this.emit('init');
   };
+
   Base.prototype = {
+    is_mockup_pattern: true,
     constructor: Base,
     on: function(eventName, eventCallback) {
       this.$el.on(eventName + '.' + this.name + '.patterns', eventCallback);
     },
-    trigger: function(eventName, args) {
+    emit: function(eventName, args) {
       // args should be a list
       if (args === undefined) {
         args = [];
@@ -28,28 +31,35 @@ define([
       this.$el.trigger(eventName + '.' + this.name + '.patterns', args);
     }
   };
+
   Base.extend = function(NewPattern) {
     var Base = this, Constructor;
-
     if (NewPattern && NewPattern.hasOwnProperty('constructor')) {
       Constructor = NewPattern.constructor;
     } else {
       Constructor = function() { Base.apply(this, arguments); };  // TODO: arguments from where
     }
-
     var Surrogate = function() { this.constructor = Constructor; };
     Surrogate.prototype = Base.prototype;
     Constructor.prototype = new Surrogate();
     Constructor.extend = Base.extend;
-
+    Constructor.init = function initMockup($el, name, options) {
+      name = name || this.prototype.name;
+      var log = logger.getLogger("pat." + name);
+      var pattern = $el.data('pattern-' + name);
+      if (pattern === undefined && Registry.patterns[name]) {
+        try {
+            pattern = new Registry.patterns[name]($el, Registry.getOptions($el, name, options));
+        } catch (e) {
+            log.error('Failed while initializing "' + name + '" pattern.');
+        }
+        $el.data('pattern-' + name, pattern);
+      }
+      return pattern;
+    };
     $.extend(true, Constructor.prototype, NewPattern);
-
     Constructor.__super__ = Base.prototype;  // TODO: needed?
-
-    // FIXME: shitty hack but we need a way to identify Mockup patterns.
-    Constructor.pattern_type = 'mockup';
     Registry.register(Constructor);
-
     return Constructor;
   };
 
